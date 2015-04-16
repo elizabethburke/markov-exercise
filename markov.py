59cf27006a0f75ee118c48625b30778f4f28de00
Hackbright Student <info@hackbrightacademy.com>
2015-04-16 16:06:43 -0700

    Leslie and Elizabeth successfully created the make_text function which takes our Markov dictionary and creates random text.

--- markov.py
+++ markov.py
@@ -1,4 +1,3 @@
-import collections
 import random
 from sys import argv
 
@@ -10,7 +9,8 @@ def make_chains(corpus):
     """Takes input text as string; returns dictionary of markov chains."""
     key_tup = tuple()
     text = corpus.read()
-    poem_word = text.rstrip().split()
+    poem_word = text.rstrip().replace(",", "").replace("?", "").split()
+    last_words = poem_word[-2] + " " + poem_word[-1]
     poem_dict = {}
     for i in range(len(poem_word) - 2):
         key_tup = (poem_word[i], poem_word[i + 1]) 
@@ -18,27 +18,33 @@ def make_chains(corpus):
             poem_dict[key_tup] = [poem_word[i + 2]]
         else:
             poem_dict[key_tup].append(poem_word[i + 2])
+    return poem_dict, last_words
 
-    return poem_dict
 
-
-def make_text(chains):
+def make_text(chains, last_words):
     """Takes dictionary of markov chains; returns random text."""
-    start_word = random.choice(chains.keys())
-    second_word = random.choice(chains[start_word])
-    print start_word
-    print second_word
+    start_words = random.choice(chains.keys())
+    second_word = random.choice(chains[start_words])
+    w1 = start_words[1]
+    w2 = second_word
+    
+    gen_words = []
+    while True:
+        gen_words.append(w1)
+        w1, w2 = w2, random.choice(chains[w1, w2])
+        if w1 + " " + w2 == last_words:
+            break
+    gen_words.append(w2)
+    print ' '.join(gen_words)
 
-    return "Here's some random text."
 
 
 
 input_text = "Some text"
 
 # Get a Markov chain
-chain_dict = make_chains(filename)
+chain_dict, last_words = make_chains(filename)
 
 # Produce random text
-random_text = make_text(chain_dict)
+random_text = make_text(chain_dict, last_words)
 
-print random_text
